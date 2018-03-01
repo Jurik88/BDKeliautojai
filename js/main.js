07dc0f5bace4488e2a371cc74e0d5b766148cc2f
Lina <lina.buineviciute@gmail.com>
2018-03-01 19:54:51 +0200

    2018-03-01 papildymai

--- js/main.js
+++ js/main.js
@@ -1,6 +1,26 @@
 //Jurij pradzia 
 $(document).ready(function(){
   $('.parallax').parallax();
+
+  var kelionesKaina = parseInt($('#pirkinioKaina').text());
+  $('#galutineKaina').val(kelionesKaina);
+
+  lightbox.option({
+    'resizeDuration': 50,
+    'wrapAround': true,
+    'alwaysShowNavOnTouchDevices' : true,
+    'maxHeight' : 400,
+  });
+
+  $("#pasirinkimas").change(function() {
+    var pasirinktasSkaicius = this.value;
+    var kelionesKaina = parseInt($('#pirkinioKaina').text());
+    var galutineKaina = pasirinktasSkaicius * kelionesKaina;
+
+    $('#galutineKaina').val(galutineKaina);
+
+  });
+
 });
 
 //Page Scrolling to ID START
@@ -42,3 +62,6 @@ setTimeout(function(){
 }, 5000);
 
 //Jurij pabaiga
+
+
+
--- js/mainCT.js
+++
@@ -1,6 +0,0 @@
-lightbox.option({
-    'resizeDuration': 50,
-    'wrapAround': true,
-    'alwaysShowNavOnTouchDevices' : true,
-    'maxHeight' : 100,
-  })
